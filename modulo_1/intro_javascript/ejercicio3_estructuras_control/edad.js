3b9126e09b53536d620fcca760dbf70507685b15
Waddley Belizaire <Norbin>
2019-08-05 11:38:10 -0400

    estructurasDeControl

---
+++ modulo_1/intro_javascript/ejercicio3_estructuras_control/edad.js
@@ -0,0 +1,13 @@
+var estudiante = {
+    nombre: "Waddley",
+    edad: 45,
+    tieneHijos: false, 
+    estaCasado: false
+}
+if (estudiante.edad > 18){
+console.log(`${estudiante.nombre} es mayor de edad`);
+}
+
+if (estudiante.edad < 18) {
+    console.log(`${estudiante.nombre} es menor de edad`);
+}
---
+++ modulo_1/intro_javascript/ejercicios2_objetos/hotel.js
@@ -0,0 +1,37 @@
+var Hotel = {
+    nombre: "Aladen PIMP Hotel",
+    rating: 12,
+    habitaciones: 23,
+    reservaciones: 13,
+    TieneGym: true
+}
+
+console.log(Hotel.nombre);
+console.log(Hotel.rating);
+console.log(Hotel.reservaciones);
+console.log(Hotel.habitaciones);
+console.log(Hotel.TieneGym);
+
+//Concatenar vs Template String //
+console.log(`Welcome 
+to paradise`);
+console.log("Bienvenido al Hotel " + Hotel.nombre + " QUE tiene a lot of STREAPERS " + Hotel.habitaciones);
+
+
+console.log("El hotel tiene " + Hotel.reservaciones + " reservaciones ");
+console.log(`El Hotel tiene ${Hotel.reservaciones} reservaciones`);
+
+console.log("El hotel tiene " + Hotel.habitaciones + " habitaciones ");
+console.log(`El Hotel tiene ${Hotel.habitaciones} habitaciones`);
+
+console.log("El tiene " + Hotel.reservaciones + " reservaciones " + " de " + Hotel.habitaciones + " habitaciones ");
+console.log(`El Hotel tiene ${Hotel.reservaciones} reservaciones ${Hotel.habitaciones} habitaciones `);
+
+console.log("El hotel tiene " + (Hotel.habitaciones - Hotel.reservaciones) + " disponibles ");
+console.log(`El Hotel tiene ${Hotel.habitaciones - Hotel.reservaciones}  disponibles `);
+
+
+document.write(`<h1> Welcome to my World </h1> 
+<p> Este Hotel es de ${Hotel.habitaciones} habitaciones </p>
+<p>Actualmente tenemos ${Hotel.habitaciones} de las cuales ${Hotel.reservaciones} reservadas </p>
+`);
