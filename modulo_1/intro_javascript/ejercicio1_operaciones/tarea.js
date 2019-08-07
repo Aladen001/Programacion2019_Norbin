f069ace5f4c567f0c37eee3a7400e92ae251c1a5
Waddley Belizaire Norbin <waddbeliz@gmail.com>
2019-08-07 12:24:40 -0400

    tarea

---
+++ modulo_1/intro_javascript/ejercicio1_operaciones/tarea.js
@@ -0,0 +1,95 @@
+/*Ej1
+var num1 = parseInt(prompt("engresar tu numero"));
+var num2 = parseInt(prompt("engresar tu numero"));
+
+var sum =num1+ num2;
+var rest = num1 - num2;
+
+console.log(sum);
+console.log(rest);
+*/
+//Ej2
+
+/*var numero1 = prompt("Digite el primer numero");
+var numero2 = prompt("Digite el segundo numero");
+
+alert(`Esos son los OP basicas A'ight:
+1.suma
+2.resta
+3.mult
+4.div
+`);
+var operacion = prompt("khf;dsklihfsid");
+//suma
+if(operacion == 1) {
+    var suma = numero1 + numero2;
+    console.log(suma);
+}
+//resta
+if(operacion == 2) {
+    var resta = numero1 - numero2;
+    console.log(resta);
+}
+//mult
+if (operacion == 3) {
+    var mult = numero1 * numero2;
+    console.log(mult);
+}
+//div
+if (operacion == 4) {
+    var div = numero1 / numero2;
+    console.log(div);
+}
+*/
+//Ej3
+/*var numero = parseInt(prompt("Digite un fucking number enfoire "));
+
+if(numero > 0) {
+   console.log("Es positivo");
+}
+else if(numero === 0){
+    console.log("This fuckin' number es zero mufucka")
+}
+else{
+     console.log("Es negativo");
+}
+*/
+//Dia 
+/*var dia = parseInt(prompt("Digite un  day to know"));
+if( dia== 1){
+    console.log("this day es Lunes");
+}
+else if (dia === 2) {
+    console.log("this day es Martes");
+}
+else if (dia === 3) {
+    console.log("this day es Miercoles");
+}
+else if (dia === 4) {
+    console.log("this day es Jueves");
+}
+else if (dia === 5) {
+    console.log("this day es Viernes");
+}
+else if (dia === 6) {
+    console.log("this day es Sabado");
+}
+else if (dia === 7) {
+    console.log("this day es Domingo");
+}
+*/
+/*var nombre = prompt ("Digite el nombre");
+var horasTrab = parseInt(prompt("Digite el horas de trabajo"));
+var pagoHoras = parseInt(prompt("Digite el pago por horas"));
+
+var ganarPorHoras = horasTrab * pagoHoras;
+console.log(`ganarPorHoras es: ${ganarPorHoras} $`);
+*/
+var nombre = prompt("Engresar un nombre");
+var año = parseInt(prompt("Digite tu año de nacimiento"));
+if(año > 0) {
+   console.log(`El año es : ${2019-año} `);
+    document.write(`El año es : ${2019-año} `);
+}
+
+    
