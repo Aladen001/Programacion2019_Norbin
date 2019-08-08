97ab2c2e62f5181ac290e94f2a82d353b3b9ed80
waddley <waddbeliz@gmail.com>
2019-08-08 12:02:36 -0400

    cajero

---
+++ modulo_1/intro_javascript/ejercicio3_estructuras_control/cajero.js
@@ -0,0 +1,40 @@
+
+/*Selecciona idioma
+prompt("Seleciona su idioma");
+//Introducir la tarjeta
+prompt("Itroduzca la tarjeta por favor");
+//Digitarr el PIN
+prompt("Digite su PIN");
+//Muestran las opciones
+prompt(`Seleciona opcion a realizar
+1.Retiro
+2.Avance
+3.Chequear Balance
+4.Deposit`);
+//Selecciono opcion
+prompt("Seleciona una opcion");
+//Seleccionar tipo de cuenta
+prompt(`Seleciona el tipo de cuenta
+1.Cuenta Ahorro
+2.
+3.`);
+//Muestra opciones de monto 
+//Seleccionomonto
+//Muestra opcion de recibo
+prompt(`Quiere ver el recibo
+1.Si
+2.No `);*/
+//Elegir si o no del comprobante del recibo
+
+//var valor = prompt ("Digita un valor");
+//console.log(typeof valor);
+
+//var foo = 'Hello World';
+//console.log(foo.length);
+//Introducir la tarjeta
+var tarjeta = prompt ("Introduzca la tarjeta,por favor");
+// Validar que la longitud de digitos sea 12
+// Mostrar un mensaje en caso que  la longitud sea mayor que 12
+if(tarjeta.lenght > 4){
+    alert("Your fucking card is declined ma nigga");
+}
