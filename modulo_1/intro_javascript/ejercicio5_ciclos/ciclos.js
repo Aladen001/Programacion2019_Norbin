a9fc9ffbb472ad7feabf9fb004a1169f9fb9dc3d
Aladen001 <waddbeliz@gmail.com>
2019-08-13 12:21:19 -0400

    ciclos

---
+++ modulo_1/intro_javascript/ejercicio5_ciclos/ciclos.js
@@ -0,0 +1,73 @@
+/* 
+for(var i= 0; i< 10; i++){
+    console.log(i);
+
+}
+
+for(var veces=1; veces<=15; veces++){
+    document.write(`<p>Soy un parrafo</p>`);
+    if(veces == 5){
+        break;
+    }
+}
+for(var veces=1; veces<=15; veces++){
+    document.write(`<p>Soy un parrafo</p>`);
+    if(veces == 5){
+        continue;
+    }
+    document.write(`<p>Soy un parrafo ${veces}</p>`);
+}
+
+for(var i=1; i<=100; i++){
+    console.log(i);
+
+var aladen = 0;
+while(aladen < 10){
+    console.log("Soy un numero");
+    aladen++;
+
+}
+    var numero= parseInt(prompt("Digite un numero"));
+    while (numero < 0){
+       var numero = parseInt(prompt("Digite un numero"));
+    }
+        alert(`El numero digitade es ${numero}`);
+        */
+       //1
+      /* var cat1 = parseInt(prompt("Digite el primer cat"));
+       var cat2 = parseInt(prompt("Digite el secundo cat"));
+
+       var potencia1 = cat1 ** 2; // cat1 * cat1
+       var potencia2 = cat1 ** 2;
+        var sumPotenscia = potencia1 + potencia2;
+        var raizCuadrada = Math.sqrt(sumPotenscia);
+        alert(`La  putain Hipotenusa es ${raizCuadrada.toFixed(1)}`);
+        */
+        //2
+        /*var nota1 = parseInt(prompt("Digite el primer numero"));
+        var nota2 = parseInt(prompt("Digite el segundo numero"));
+        var nota3 = parseInt(prompt("Digite el tercera numero"));
+        var nota4 = parseInt(prompt("Digite el cuarta numero"));
+        var prom = nota1 + nota2 + nota3 + nota4;
+        console.log(prom / 4);
+        */
+        //3
+        /*var velocidad = parseFloat(prompt("Digite la velocidad"));
+        var tiempo = parseFloat(prompt("Digtar el tiempo"));
+        var distencnia;
+        distencia = velocidad * tiempo;
+        alert(`La distencia recorida es ${distencia}`);
+        */
+        //4
+            /*var numero = parseInt(prompt("Digitar un numero para ver si es cierto"));
+            for(numero = 1; numero<=100; numero++){
+                console.log(numero);
+            }*/
+            //10 
+            /*for(var num= 1; num<=100; num++ ){
+                //imp los pares
+                if(num %2 ==0){
+                    console.log(num);
+                }
+            }*/
+            
